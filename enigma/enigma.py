247f8155355c1c6964b739eca3a7079108fa7b54
Alireza (arman-async) <armanparsi206@outlook.com>
2024-09-12 03:13:34 +0330

    added Base Enigma Class

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -1,5 +1,7 @@
 from typing import *
-import time
+from pathlib import Path
+
+BASE64 = "0123456789+/ABCDEFGHIJKLMNOPQRSTUVWXYZ=abcdefghijklmnopqrstuvwxyz"
 
 class Rotor:
     def __init__(self, rotor: List[str], offset: int = 0, rotate_count: int = 1) -> None:
@@ -49,3 +51,25 @@ class Reflector:
     
     def __len__(self) -> int:
         return len(self.reflector)
+
+
+class Enigma:
+    def __init__(self, rotors: List[Rotor], password: str, reflector:Reflector=None) -> None:
+        self.rotors: List[Rotor] = rotors
+        self.reflector: Reflector = reflector if reflector else Reflector(BASE64)
+        self.password: str = password
+        self._rotor_count: int = len(self.rotors)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}<rotor_count: {self._rotor_count}>"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def __len__(self) -> int:
+        return self._rotor_count
+
+    def encrypt(self, string: str) -> str:
+        pass
+
+
