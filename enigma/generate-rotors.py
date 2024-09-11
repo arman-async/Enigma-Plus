76333b79cfd0aa1c2e3469d1edd7ae4ad761ac39
Alireza (arman-async) <armanparsi206@outlook.com>
2024-09-12 01:49:04 +0330

    Start...

---
+++ enigma/generate-rotors.py
@@ -0,0 +1,25 @@
+import argparse
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-rc", "--rotor-count", type=int,
+    help="number of rotors to generate",
+    default=28,
+)
+parser.add_argument(
+    '-o', '--output', type=str,
+    help="output file",
+    default="new.rotors",
+)
+
+args = parser.parse_args()
+
+BASE64 = "0123456789+/ABCDEFGHIJKLMNOPQRSTUVWXYZ=abcdefghijklmnopqrstuvwxyz"
+def random_rotor(charset:str=BASE64)-> str:
+    return "".join(random.choices(charset, k=len(charset)))
+
+with open(args.output, 'w') as f:
+    for _ in range(args.rotor_count):
+        rotor = f'{random_rotor()};'
+        f.write(rotor)
